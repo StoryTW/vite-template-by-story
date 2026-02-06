4b65bfe4e89ff8ad5588012a8d599f4452beb74f
StoryTW <sergey451495@mail.ru>
2026-02-06 14:15:16 +0300

    fix

--- index.tsx
+++ index.tsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from '@/App';
 
 import 'modern-normalize/modern-normalize.css';
-import '@/assets/styles/_index.scss';
+import '@/assets/styles/index.scss';
 
 createRoot(document.getElementById('root')!).render(<App />);
