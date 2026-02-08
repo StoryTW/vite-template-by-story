bcc13e4186bacc09b46ad0ff212bc64fae15e2fc
StoryTW <sergey451495@mail.ru>
2026-02-08 03:48:38 +0300

    add split chunks

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,32 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config
 export default defineConfig({
   plugins: [react(), svgr()],
+
   resolve: {
     alias: {
       '@': path.resolve('./src'),
     },
   },
+
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@use "@/assets/styles/mixins.scss" as *;`,
+      },
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react';
+            if (id.includes('axios')) return 'http';
+
+            return 'vendor';
+          }
+        },
       },
     },
   },
